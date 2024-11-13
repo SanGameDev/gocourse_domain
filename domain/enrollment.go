10b44578beffc315e74036286c48e4677b6ec480
Santiago Vaca <santiago.gamedevs@gmail.com>
2024-11-13 08:04:22 -0600

    changes

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -8,14 +8,14 @@ import (
 )
 
 type Enrollment struct {
-	ID        string     `json:"id"`
-	UserID    string     `json:"user_id"`
-	User      User       `json:"user"`
-	CourseID  string     `json:"course_id"`
-	Course    Course     `json:"course"`
-	Status    string     `json:"status"`
-	CreatedAt *time.Time `json:"created_at"`
-	UpdatedAt *time.Time `json:"updated_at"`
+	ID        string       `json:"id" gorm:"type:char(36);not null;primary_key;unique_index"`
+    UserID    string       `json:"user_id,omitempty" gorm:"type:char(36)"`
+    User      *User        `json:"user,omitempty" gorm:"-"`
+    CourseID  string       `json:"course_id" gorm:"type:char(36);not null"`
+    Course    *Course      `json:"course,omitempty" gorm:"-"`
+    //Status    EnrollStatus `json:"status" gorm:"type:char(2)"`
+    CreatedAt *time.Time   `json:"-"`
+    UpdatedAt *time.Time   `json:"-"`
 }
 
 func (c *Enrollment) BeforeCreate(tx *gorm.DB) (err error) {
